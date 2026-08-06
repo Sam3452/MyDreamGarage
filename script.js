c2bb9a80fffc3b9b03daa3ec631827dd237a1f60
Sam3452 <sam.j.brooks@hotmail.com>
2026-08-06 23:02:48 +0100

    added info boxes, added examples images

--- script.js
+++ script.js
@@ -9,22 +9,47 @@ async function getCars() {
             const carBox = document.createElement('div');
             carBox.classList.add('car-box');
 
-            carBox.innerHTML = `
-            <img src="${car.image}" alt="${car.name}">
-            <h2>${car.manufacturer}</h2>
-            <p>${car.model}</p>
-            <p>${car.generation}</p>
-            <p>Trim Levels: ${car.trim_levels.join(', ')}</p>
-            <p>Specials: ${car.specials.join(', ')}</p>
-            <p>Engine Options: ${car.engine_options.join(', ')}</p>
-            `;
+            carBox.innerHTML = `<img src="${car.image}" alt="${car.manufacturer} ${car.model}">`;
+
+            carBox.addEventListener('click', () => openInfo(car));
 
             container.appendChild(carBox);
+    
         });
     } catch (error) {
         console.error('Error getting cars:', error);
 
     }
 }
+
+function openInfo(car) {
+    const infoBox = document.getElementById('car-info');
+    const infoContent = document.getElementById('info-content');
+
+      infoContent.innerHTML = `
+        <span id="closeInfo">&times;</span>
+        <img src="${car.image}" alt="${car.manufacturer} ${car.model}">
+        <h2>${car.manufacturer} ${car.model}</h2>
+        <p>${car.generation}</p>
+        <p>Colours:${car.colours.join(', ')}</p>
+        <p>Trim Levels: ${car.trim_levels.join(', ')}</p>
+        <p>Specials: ${car.specials.join(', ')}</p>
+        <p>Engine Options: ${car.engine_options.join(', ')}</p>
+    `;
+
+    infoBox.style.display = 'flex';
+
+    document.getElementById('closeInfo').addEventListener('click', closeInfo);
+    
+}
+
+function closeInfo() {
+    document.getElementById('car-info').style.display = 'none';
+}
+
+document.addEventListener('click', (e) => {
+    const infoBox = document.getElementById('car-info');
+    if (e.target === infoBox) closeInfo();
+});
 
 getCars();
