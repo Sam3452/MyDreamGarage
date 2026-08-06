6ac0e0eb00d566d39430f2429d62f4df34162e32
Sam3452 <sam.j.brooks@hotmail.com>
2026-08-06 16:35:26 +0100

    added some examples in json file, fixed the typo in the script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function getCars() {
         const response = await fetch('cars.json');
         const cars = await response.json();
 
-        const container = document.getElementId('car-container');
+        const container = document.getElementById('car-container');
 
         cars.forEach(car => {
             const carBox = document.createElement('div');
@@ -13,6 +13,10 @@ async function getCars() {
             <img src="${car.image}" alt="${car.name}">
             <h2>${car.manufacturer}</h2>
             <p>${car.model}</p>
+            <p>${car.generation}</p>
+            <p>Trim Levels: ${car.trim_levels.join(', ')}</p>
+            <p>Specials: ${car.specials.join(', ')}</p>
+            <p>Engine Options: ${car.engine_options.join(', ')}</p>
             `;
 
             container.appendChild(carBox);
