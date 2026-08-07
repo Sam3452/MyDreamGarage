70cf604d04243b27fbb1f43a5d62dc88c5b9ee96
Sam3452 <sam.j.brooks@hotmail.com>
2026-08-07 19:34:37 +0100

    added some more example data

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import http.server
 import socketserver
 import sys
 
-PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
+PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 5500
 
 Handler = http.server.SimpleHTTPRequestHandler
 
