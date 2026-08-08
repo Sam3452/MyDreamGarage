bdbe0800da9b0ed4c9d1781a7bd89dcd16db535f
Sam3452 <sam.j.brooks@hotmail.com>
2026-08-08 12:46:00 +0100

    created the db setup script

---
+++ setup_db.py
@@ -0,0 +1,43 @@
+import sqlite3
+import json
+
+conn = sqlite3.connect('cars.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS cars (
+id INTEGER PRIMARY KEY,
+manufacturer TEXT,
+model TEXT,
+generation TEXT,
+colours TEXT,
+trim_levels TEXT,
+specials TEXT,
+engine_options TEXT,
+image TEXT
+)
+''')
+
+with open('cars.json') as f:
+    cars = json.load(f)
+
+for car in cars:
+    cursor.execute('''
+INSERT OR REPLACE cars (id, manufacturer, model, generation, colours, trim_levels, specials, engine_options, image)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+''', (
+    car['id'],
+    car['manufacturer'],
+    car['model'],
+    car['generation'],
+    json.dumps(car['colours']),
+    json.dumps(car['trim_levels']),
+    json.dumps(car['specials']),
+    json.dumps(car['engine_options']),
+    car['image']
+))
+
+    conn.commit()
+    conn.close()
+    print("Datbase Created and populated")
+    
